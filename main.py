6d2c1f5475853781b7f152dc62d1b32b0e1f5772
Jay Wren <jawren@cisco.com>
2024-09-30 09:59:07 -0400

    my part 4

--- main.py
+++ main.py
@@ -1,74 +1,29 @@
 # https://langchain-ai.github.io/langgraph/tutorials/introduction/#part-1-build-a-basic-chatbot
-import getpass
-import os
 
-
-# def _set_env(var: str):
-#     if not os.environ.get(var):
-#         os.environ[var] = getpass.getpass(f"{var}: ")
-
-
-# _set_env("ANTHROPIC_API_KEY")
 
 from typing import Annotated
-
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
 from typing_extensions import TypedDict
 
-from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
+memory = MemorySaver()
 
 
 class State(TypedDict):
-    # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
 
 
 graph_builder = StateGraph(State)
 
 
-#### part #2
-from langchain_community.tools.tavily_search import TavilySearchResults
-
 tool = TavilySearchResults(max_results=2)
 tools = [tool]
 #tool.invoke("What's a 'node' in LangGraph?")
-
-import json
-
-from langchain_core.messages import ToolMessage
-
-
-class BasicToolNode:
-    """A node that runs the tools requested in the last AIMessage."""
-
-    def __init__(self, tools: list) -> None:
-        self.tools_by_name = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs: dict):
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No message found in input")
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"]
-            )
-            outputs.append(
-                ToolMessage(
-                    content=json.dumps(tool_result),
-                    name=tool_call["name"],
-                    tool_call_id=tool_call["id"],
-                )
-            )
-        return {"messages": outputs}
-
-
-tool_node = BasicToolNode(tools=[tool])
-graph_builder.add_node("tools", tool_node)
-
 
 # from langchain_anthropic import ChatAnthropic
 
@@ -90,55 +45,27 @@ def chatbot(state: State):
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
 
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
 
-graph_builder.add_edge("chatbot", END)
-
-# part 2
-from typing import Literal
-
-
-def route_tools(
-    state: State,
-) -> Literal["tools", "__end__"]:
-    """
-    Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-    """
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in input state to tool_edge: {state}")
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-    return "__end__"
-
-
-# The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "__end__" if
-# it is fine directly responding. This conditional routing defines the main agent loop.
 graph_builder.add_conditional_edges(
     "chatbot",
-    route_tools,
-    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
-    # It defaults to the identity function, but if you
-    # want to use a node named something else apart from "tools",
-    # You can update the value of the dictionary to something else
-    # e.g., "tools": "my_tools"
-    {"tools": "tools", "__end__": "__end__"},
+    tools_condition,
 )
-# Any time a tool is called, we return to the chatbot to decide the next step
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
-# part 3
 
-from langgraph.checkpoint.memory import MemorySaver
+graph = graph_builder.compile(
+    checkpointer=memory,
+    # This is new!
+    interrupt_before=["tools"],
+    # Note: can also interrupt __after__ tools, if desired.
+    # interrupt_after=["tools"]
+)
 
-memory = MemorySaver()
-graph = graph_builder.compile(checkpointer=memory)
 
 config = {"configurable": {"thread_id": "1"}}
-user_input = "Hi there! My name is Jay R Wren."
+user_input = "I'm learning LangGraph. Could you do some research on it for me?"
 
 # The config is the **second positional argument** to stream() or invoke()!
 events = graph.stream(
@@ -146,6 +73,18 @@ events = graph.stream(
 )
 for event in events:
     event["messages"][-1].pretty_print()
+
+snapshot = graph.get_state(config)
+print(snapshot.next)
+existing_message = snapshot.values["messages"][-1]
+print(existing_message.tool_calls)
+
+events = graph.stream(None, config, stream_mode="values")
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
+
+exit()
 
 user_input = "Remember my name?"
 
@@ -157,8 +96,8 @@ events = graph.stream(
 for event in events:
     event["messages"][-1].pretty_print()
 
-snapshot = graph.get_state(config)
-print(snapshot)
+#snapshot = graph.get_state(config)
+#print(snapshot)
 
 exit()
 
