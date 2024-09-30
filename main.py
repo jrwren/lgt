8c6e8c56fca4ee8da086e4a5221eb964f355cac6
Jay Wren <jawren@cisco.com>
2024-09-30 17:02:22 -0400

    part 6 doesnt work I am not sure why. there is also this typo fix: https://github.com/langchain-ai/langgraph/pull/1924 I am not impressed

--- main.py
+++ main.py
@@ -2,22 +2,30 @@
 
 from typing import Annotated
 
-from langchain_anthropic import ChatAnthropic
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import AIMessage, ToolMessage
+from pydantic import BaseModel
 from typing_extensions import TypedDict
 
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+    # This flag is new
+    ask_human: bool
 
 
-graph_builder = StateGraph(State)
+class RequestAssistance(BaseModel):
+    """Escalate the conversation to an expert. Use this if you are unable to assist directly or if the user requires support beyond your permissions.
+
+    To use this function, relay the user's 'request' so the expert can provide the right guidance.
+    """
+
+    request: str
 
 
 tool = TavilySearchResults(max_results=2)
@@ -25,24 +33,68 @@ tools = [tool]
 from langchain_ollama.chat_models import ChatOllama
 llm = ChatOllama(base_url='m1.local:11434', model="llama3.2")
 # Modification: tell the LLM which tools it can call
-llm_with_tools = llm.bind_tools(tools)
+llm_with_tools = llm.bind_tools(tools + [RequestAssistance])
 
 
 def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+    # was: return {"messages": [llm_with_tools.invoke(state["messages"])]}
+    response = llm_with_tools.invoke(state["messages"])
+    ask_human = False
+    if (
+        response.tool_calls
+        and response.tool_calls[0]["name"] == RequestAssistance.__name__
+    ):
+        ask_human = True
+    return {"messages": [response], "ask_human": ask_human}
 
 
+graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=[tool]))
 
-tool_node = ToolNode(tools=[tool])
-graph_builder.add_node("tools", tool_node)
+
+def create_response(response: str, ai_message: AIMessage):
+    return ToolMessage(
+        content=response,
+        tool_call_id=ai_message.tool_calls[0]["id"],
+    )
+
+
+def human_node(state: State):
+    new_messages = []
+    if not isinstance(state["messages"][-1], ToolMessage):
+        # Typically, the user will have updated the state during the interrupt.
+        # If they choose not to, we will include a placeholder ToolMessage to
+        # let the LLM continue.
+        new_messages.append(
+            create_response("No response from human.", state["messages"][-1])
+        )
+    return {
+        # Append the new messages
+        "messages": new_messages,
+        # Unset the flag
+        "ask_human": False,
+    }
+
+
+graph_builder.add_node("human", human_node)
+
+def select_next_node(state: State):
+    if state["ask_human"]:
+        return "human"
+    # Otherwise, we can route as before
+    return tools_condition(state)
+
 
 graph_builder.add_conditional_edges(
     "chatbot",
-    tools_condition,
+    select_next_node,
+    {"human": "human", "tools": "tools", "__end__": "__end__"},
 )
 graph_builder.add_edge("tools", "chatbot")
-graph_builder.set_entry_point("chatbot")
+graph_builder.add_edge("human", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+#graph_builder.set_entry_point("chatbot")
 
 memory = MemorySaver()
 graph = graph_builder.compile(
@@ -54,74 +106,31 @@ graph = graph_builder.compile(
 )
 
 
+user_input = "I need some expert guidance for building this AI agent. Could you request assistance for me?"
 config = {"configurable": {"thread_id": "1"}}
-user_input = "I'm learning LangGraph. Could you do some research on it for me?"
-
 # The config is the **second positional argument** to stream() or invoke()!
 events = graph.stream(
     {"messages": [("user", user_input)]}, config, stream_mode="values"
 )
 for event in events:
-    event["messages"][-1].pretty_print()
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
 
+# NOPE! THIS IS NOT WORKING!
 snapshot = graph.get_state(config)
-# start part 5 addition
-existing_message = snapshot.values["messages"][-1]
-from langchain_core.messages import AIMessage, ToolMessage
+print(snapshot.next)
 
-answer = (
-    "LangGraph is a library for building stateful, multi-actor applications with LLMs."
+ai_message = snapshot.values["messages"][-1]
+human_response = (
+    "We, the experts are here to help! We'd recommend you check out LangGraph to build your agent."
+    " It's much more reliable and extensible than simple autonomous agents."
 )
-new_messages = [
-    # The LLM API expects some ToolMessage to match its tool call. We'll satisfy that here.
-    ToolMessage(content=answer, tool_call_id=existing_message.tool_calls[0]["id"]),
-    # And then directly "put words in the LLM's mouth" by populating its response.
-    AIMessage(content=answer),
-]
-
-new_messages[-1].pretty_print()
-graph.update_state(
-    # Which state to update
-    config,
-    # The updated values to provide. The messages in our `State` are "append-only", meaning this will be appended
-    # to the existing state. We will review how to update existing messages in the next section!
-    {"messages": new_messages},
-)
-
-print("\n\nLast 2 messages;")
-print(graph.get_state(config).values["messages"][-2:])
-# end part 5 addition
+tool_message = create_response(human_response, ai_message)
+graph.update_state(config, {"messages": [tool_message]})
+msg = graph.get_state(config).values["messages"]
+print (msg)
 
 events = graph.stream(None, config, stream_mode="values")
 for event in events:
     if "messages" in event:
         event["messages"][-1].pretty_print()
-
-exit()
-
-user_input = "Remember my name?"
-
-# The config is the **second positional argument** to stream() or invoke()!
-# config = {"configurable": {"thread_id": "2"}} different thread id loses that context.
-events = graph.stream(
-    {"messages": [("user", user_input)]}, config, stream_mode="values"
-)
-for event in events:
-    event["messages"][-1].pretty_print()
-
-#snapshot = graph.get_state(config)
-#print(snapshot)
-
-exit()
-
-from langchain_core.messages import BaseMessage
-while True:
-    user_input = input("User: ")
-    print("User: "+ user_input)
-    if user_input.lower() in ["quit", "exit", "q"]:
-        print("Goodbye!")
-        break
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            if isinstance(value["messages"][-1], BaseMessage):
-                print("Assistant:", value["messages"][-1].content)
